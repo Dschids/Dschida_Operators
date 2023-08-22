42645ca13c68e9c21032586f991f9ce25e27915e
Bob Dschida <rdschida@students.mchenry.edu>
2023-08-22 16:10:44 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    // variables
+    var a = 25
+    var b = 8
+    var c = 11
+    var d = 2
+    // equations in print statements
+    println("$a + $b = ${a + b}")
+    println("$c * $b = ${c * b}")
+    println("$d > $a = ${d > a}")
+    println("$a % $d = ${a % b}")
+    println("$a / $c = ${(a / c)}")
+}
